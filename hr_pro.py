822a75d49c05a7f351efb5f29df19797d0cdb555
Lateefa Albalool <albalool.sec.com>
2022-10-02 18:42:13 +0300

    Create employee and manager classes

--- hr_pro.py
+++ hr_pro.py
@@ -1,11 +1,27 @@
 class Employee:
-   #Employee class here
+    def __init__(self, name, age, salary, employment_years):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.employment_years = employment_years
+
+    def get_annual_salary(self, salary):
+        return self.salary * 12
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Working Years: {self.employment_years}"
 
 class Manager(Employee):
-    #Manager class here
-        
-def main():
-	# main code here
+    def __init__(self, name, age, salary, employment_years, bonus_percentage):
+        super().__init__(name, age, salary, employment_years)
+        self.bonus_percentage = bonus_percentage
 
-if __name__ == '__main__':
-	main()
+    def __str__(self):
+        return f"Name: {Employee.name}, Age: {Employee.age}, Salary: {Employee.salary}, Working Years: {Employee.employment_years}, Bounus: {self.bonus_percentage}"
+
+    
+#     def main():
+# 	# main code here
+
+# # if __name__ == '__main__':
+# # 	main()
